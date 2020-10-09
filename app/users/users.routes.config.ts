fe590a7ff06981bddb385c5f4cca422fb638c77b
m0ai <frozenmoai@gmail.com>
2020-10-10 02:38:02 +0900

    Append a validate middleware

--- app/users/users.routes.config.ts
+++ app/users/users.routes.config.ts
@@ -1,7 +1,8 @@
+import express from 'express'
+
 import { CommonRoutesConfig, configureRoutes } from '../common/common.routes.config'
 import { UsersController } from './controllers/users.controller';
-
-import express from 'express'
+import { UsersMiddleware } from './middlewares/users.middleware';
 
 
 export class UsersRoutes extends CommonRoutesConfig implements configureRoutes {
@@ -12,28 +13,39 @@ export class UsersRoutes extends CommonRoutesConfig implements configureRoutes {
 
   configureRoutes() {
     const usersController = new UsersController()
+    const usersMiddleware = new UsersMiddleware()
 
     this.app.get(`/users`, [
-      usersController.listUsers
+      usersController.listUsers,
     ])
 
     this.app.post(`/users`, [
-      usersController.createUser
+      usersMiddleware.validateRequiredCreateUserBodyFields,
+      usersMiddleware.validateSameEmailDoesntExist,
+      usersController.createUser,
     ])
 
     this.app.put(`/users/:userId`, [
+      usersMiddleware.validateUserExist,
+      usersMiddleware.extractUserId,
       usersController.put
     ])
 
     this.app.patch(`/users/:userId`, [
+      usersMiddleware.validateUserExist,
+      usersMiddleware.extractUserId,
       usersController.patch
     ])
 
     this.app.delete(`/users/:userId`, [
+      usersMiddleware.validateUserExist,
+      usersMiddleware.extractUserId,
       usersController.removeUser
     ])
 
     this.app.get(`/users/:userId`, [
+      usersMiddleware.validateUserExist,
+      usersMiddleware.extractUserId,
       usersController.getUserById
     ])
   }
