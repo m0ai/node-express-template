61ea63c2b25cc600b5fa15d3024f3e13a84bbf9e
m0ai <frozenmoai@gmail.com>
2020-10-09 23:39:49 +0900

    Add a body parser middleware to express app

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,15 @@ import * as http from 'http'
 import { CommonRoutesConfig } from './common/common.routes.config'
 import { UsersRoutes } from './users/users.routes.config'
 
+
 const app: express.Application = express()
 const server: http.Server = http.createServer(app)
 const port = 3000
 const routes: any = []
+
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({extended: false}));
+
 routes.push(new UsersRoutes(app))
 
 
