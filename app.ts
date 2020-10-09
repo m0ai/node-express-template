2f73acf646343de5739cc0c487d1dcc7442951f3
m0ai <frozenmoai@gmail.com>
2020-10-09 18:47:55 +0900

    Add a mocking users controller and routes configure

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-import express from 'express';
-import * as http from 'http';
+import express from 'express'
+import * as http from 'http'
 
-const app: express.Application = express();
-const server: http.Server = http.createServer(app);
-const port = 3000;
+import { CommonRoutesConfig } from './common/common.routes.config'
+import { UserRoutes } from './users/users.routes.config'
+
+const app: express.Application = express()
+const server: http.Server = http.createServer(app)
+const port = 3000
+const routes: any = []
+routes.push(new UserRoutes(app))
+
 
 app.get('/', (req: express.Request, res: express.Response) =>{
     res.status(200).send(`Server running at port ${port}`)
@@ -11,4 +17,7 @@ app.get('/', (req: express.Request, res: express.Response) =>{
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
+    routes.forEach((route: CommonRoutesConfig) => {
+        console.log(`Routes configured for ${route.getName()}`)
+    });
 });
